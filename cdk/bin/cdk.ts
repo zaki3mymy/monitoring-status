a50cd8f9fca039fa24f8a0db48bb0083a5d1d2fa
zaki3mymy <zaki.last@gmail.com>
2024-01-02 10:33:01 +0000

    feat: CDK implements

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { CdkStack } from '../lib/cdk-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+const projectName = "monitoring-status";
+
+new CdkStack(app, `${projectName}-stack`, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +20,5 @@ new CdkStack(app, 'CdkStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  projectName
 });
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,56 @@
 import * as cdk from 'aws-cdk-lib';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export interface CustomizedProps extends cdk.StackProps {
+  projectName: string;
+}
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: CustomizedProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // IAM
+    const iamRoleForLambda = new iam.Role(this, "iamRoleForLambda", {
+      roleName: `${props.projectName}-lambda-role`,
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        {
+          "managedPolicyArn": "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+        }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda
+    const pythonPackagePath = "../src/" + props.projectName.replace(/-/g, "_");
+    const lambdaFunctions = new lambda.Function(this, "lambdaFunction", {
+      functionName: `${props.projectName}-lambda`,
+      runtime: lambda.Runtime.PYTHON_3_12,
+      timeout: cdk.Duration.seconds(900),
+      code: lambda.Code.fromAsset(pythonPackagePath),
+      handler: "lambda_handler.lambda_function",
+      role: iamRoleForLambda,
+      environment: {
+        "LOGLEVEL": "INFO",
+        "SECRET_KEY": "secret_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+      }
+    })
+
+    // EventBridge
+    new events.Rule(this, "eventBridge", {
+      schedule: events.Schedule.cron({minute: "0"}),
+      targets: [new targets.LambdaFunction(lambdaFunctions, {
+        event: events.RuleTargetInput.fromObject({
+          DATABASE_ID: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+          PROPERTY_NAME_STATUS: "Status",
+          PROPERTY_VALUE_STATUS_DONE: "Done",
+          PROPERTY_NAME_PUBLISH: "Publish",
+          PROPERTY_NAME_PUBLISH_DATE: "PublishDate"
+        })
+      })]
+    })
   }
 }
